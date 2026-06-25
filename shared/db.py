2d6bbc3edf0a5bd5cdd85c60106e0946aebd646d
Jakub Gulinski <gulinskijakub@gmail.com>
2026-06-25 19:45:47 +0200

    comment

--- shared/db.py
+++ shared/db.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import sessionmaker
 from shared.config import DATABASE_URL
 from shared.serialization import to_json
 
-# json_serializer so JSONB columns can hold Decimal/UUID/datetime (e.g. market
-# data raw_payload), serialised consistently with the rest of the stack.
 engine = create_engine(DATABASE_URL, pool_pre_ping=True, json_serializer=to_json)
 SessionFactory = sessionmaker(bind=engine)
         
